4890d5bc08bc22b6f96029886a3de7d36601534b
Eliav Yair <77952937+EliavYair1@users.noreply.github.com>
2022-08-07 00:38:47 +0300

    Delete src/common directory

--- src/common/Input/types.tsx
+++
@@ -1,8 +0,0 @@
-export interface InputProps {
-  value?: string;
-  placeholder?: string;
-  title?: string;
-  error?: string;
-  type?: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
